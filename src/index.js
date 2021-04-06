089d35221e7298c9656629ad3238f8fda8460ffa
André van Delft <andrevandelft@outlook.com>
2021-04-06 18:24:40 +0200

    Solidify footer

--- src/index.js
+++ src/index.js
@@ -19,22 +19,3 @@ ScrollReveal().reveal('.slideup', slideUp);
 library.add(faInstagram);
 library.add(faSpotify);
 dom.watch();
-
-// Get the header
-var scroller = document.getElementById("Scroller");
-var footer = document.getElementById("Footer");
-
-// Get the offset position of the navbar
-var sticky = footer.offsetTop + footer.offsetHeight;
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  var rect = footer.getBoundingClientRect();
-  if (rect.bottom < window.innerHeight) {
-    footer.classList.add("sticky");
-  } else {
-    footer.classList.remove("sticky");
-  }
-}
-
-scroller.addEventListener("scroll", myFunction)
