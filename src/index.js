3fe58c07804b092fdea17153b5c0a6db1914e214
André van Delft <andrevandelft@outlook.com>
2021-04-07 23:09:24 +0200

    Add webkit sticky and remove overflow scrolling

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import ScrollReveal from 'scrollreveal';
 import './scss/index.scss';
 
-import { library, dom } from '@fortawesome/fontawesome-svg-core';
-import { faInstagram, faSpotify } from '@fortawesome/free-brands-svg-icons';
-
 var slideUp = {
   delay: 200,
   duration: 1500,
@@ -15,7 +12,3 @@ var slideUp = {
 };
 
 ScrollReveal().reveal('.slideup', slideUp);
-
-library.add(faInstagram);
-library.add(faSpotify);
-dom.watch();
